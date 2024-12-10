f242b50494b23863a86858c354564f8a1de048b1
np05cp4a220020@iic.edu.np <angilneupane222@gmail.com>
2024-12-10 19:57:23 +0545

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-extralight flex justify-center align-middle">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
