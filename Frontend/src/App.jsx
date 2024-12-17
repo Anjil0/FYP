26cad7ea03c5ab8ad8bb205f5117bcfb1acf728b
np05cp4a220020@iic.edu.np <angilneupane222@gmail.com>
2024-12-18 00:58:55 +0545

    App.jsx Change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-extralight flex justify-center align-middle">
+      <h1 className="text-3xl font-extralight text-green-400 flex justify-center align-middle">
         Hello world!
       </h1>
     </>
